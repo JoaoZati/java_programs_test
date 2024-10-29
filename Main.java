a3a665737a5f0797a5d89bdf8350aa9939f4a1a2
João Guilherme <jzati@mitsidi.com>
2024-10-29 16:43:16 -0300

    Iniciando um projeto java do zero

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java in VS Code!");
+        int result = add(5, 10);
+        System.out.println("The result is: " + result);
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
